973bc1e37b05dd628d3f96096243e14c063bb63b
Pedro Tashima <pedrotashima42@gmail.com>
2021-05-21 08:04:14 -0300

    chore: updated windows install script

--- windows/install.ps1
+++ windows/install.ps1
@@ -2,10 +2,5 @@ Write-Host "Installing Chocolatey..."
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Write-Host "Completed"
 Write-Host "Installing Apps..."
-choco install -y git googlechrome 7zip.install vscode pia discord steam nvm postman barrier joplin whatsapp
-choco install docker-desktop --pre 
-Write-Host "Completed"
-Write-Host "Installing node, npm and yarn"
-nvm install latest
-npm install -g yarn
+choco install -y git firefox 7zip.install vscode nvm brave
 Write-Host "Completed"
